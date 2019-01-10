dff0bb09f4ec2c051aa744260bf4aba75e45c35e
David Hale <david.hale7@gmail.com>
2019-01-10 11:26:57 -0800

    first commit

---
+++ potusScraper.js
@@ -0,0 +1,8 @@
+const rp = require('request-promise');
+const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
+
+rp(url).then(function(html) {
+  console.log(html);
+}).catch(function() {
+  console.log('error');
+});
