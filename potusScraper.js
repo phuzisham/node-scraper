dcdcc36a62638dff72c3a0c7994339b49acd0b48
David Hale <david.hale7@gmail.com>
2019-01-10 15:13:58 -0800

    parse data

--- potusScraper.js
+++ potusScraper.js
@@ -1,8 +1,13 @@
 const rp = require('request-promise');
+const $ = require('cheerio');
 const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
+let wikiUrls = [];
 
 rp(url).then(function(html) {
-  console.log(html);
+  Array.from($('big > a', html)).forEach(function(line) {
+    wikiUrls.push(line.attribs.href);
+  });
+  console.log(wikiUrls);
 }).catch(function() {
   console.log('error');
 });
